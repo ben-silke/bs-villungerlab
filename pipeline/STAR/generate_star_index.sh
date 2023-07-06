cb64112a7df964e799a605d475b92b7f3af9743d
bensilke <u6675274@anu.edu.au>
2023-07-06 16:23:41 +0200

    updated star index generation script

--- pipeline/STAR/generate_star_index.sh
+++ pipeline/STAR/generate_star_index.sh
@@ -1,10 +1,10 @@
 
 # Index generation
-STAR \
---runThreadN 2 \
---runMode genomeGenerate \
---genomeDir ../../data/references/STAR \
---genomeFastaFiles ../../data/references/human_index_salmon/gentrome.fa
-
+# STAR \
+# --runThreadN 2 \
+# --runMode genomeGenerate \
+# --genomeDir ../../data/references/STAR \
+# --genomeFastaFiles ../../data/references/human_index_salmon/gentrome.fa
 
 # Running mapping
+STAR --runThreadN 2 --runMode genomeGenerate --genomeDir data/references/STAR --genomeFastaFiles data/references/human_index_salmon/gentrome.fa
