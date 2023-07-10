cf9318349e3863bca96e979b2b32606ac03e169c
bensilke <u6675274@anu.edu.au>
2023-07-10 12:57:04 +0200

    updates star indexing script

--- pipeline/STAR/generate_star_index.sh
+++ pipeline/STAR/generate_star_index.sh
@@ -6,19 +6,22 @@
 # --genomeDir ../../data/references/STAR \
 # --genomeFastaFiles ../../data/references/human_index_salmon/gentrome.fa
 
+# /Volumes/bs_external/villunger/references/STAR/ensembl_genome/combined_reference.fa
+
 # Running mapping
 external_drive="/Volumes/bs_external/villunger/references"
 echo "$external_drive"
 
 # Download the ensembl genome
-rsync -av rsync://ftp.ensembl.org/ensembl/pub/release-109/fasta/homo_sapiens/dna "$external_drive/STAR/ensembl_genome"
+# rsync -av rsync://ftp.ensembl.org/ensembl/pub/release-109/fasta/homo_sapiens/dna "$external_drive/STAR/ensembl_genome"
 # Download the annotation
-rsync -av rsync://ftp.ensembl.org/ensembl/pub/release-109/gff3/homo_sapiens "$external_drive/STAR/ensemble_annotation"
+# rsync -av rsync://ftp.ensembl.org/ensembl/pub/release-109/gff3/homo_sapiens "$external_drive/STAR/ensemble_annotation"
 
+# merge the cDNA and the ncRNA
 
 STAR \
 --runThreadN 2 \
 --runMode genomeGenerate \
 --genomeDir "$external_drive/STAR/star_index" \
---genomeFastaFiles "$external_drive/STAR/ensembl_genome/combinded.fa" \
+--genomeFastaFiles "$external_drive/STAR/ensembl_genome/combined_reference.fa" \
 --sjdbGTFfile "$external_drive/STAR/ensemble_annotation" \
