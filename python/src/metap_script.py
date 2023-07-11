4d93e0b0f974cc55fb93920b5746b80218342d42
bensilke <u6675274@anu.edu.au>
2023-07-11 17:19:47 +0200

    fixed metaprogramming scripts

--- python/src/metap_script.py
+++ python/src/metap_script.py
@@ -1,4 +1,4 @@
-from . import RFileWriter
+from metaprogramming_r import RFileWriter
 
 
 import click
@@ -7,11 +7,15 @@ import click
 @click.option('--treatment', '-t', help='treatment (e.g. ZM)')
 @click.option('--directory', '-d', help='Where should the files be saved?')
 @click.option('--file_location', '-f', default="data_directory = file.path(getwd(), glue('data/organised/{treatment}/output_salmon'))", help="Where are the files?")
-@click.option('--all_replicates', '-r', flag=True, help="include all replicates? or just 1-3?")
+@click.option('--all_replicates', '-r', is_flag=True, help="include all replicates? or just 1-3?")
 
 def create_files(treatment, directory, file_location, all_replicates):
     file_writer = RFileWriter(treatment, directory, file_location, all_replicates)
     file_writer.write_r_file()
-    file_writer.write_markdown_file
+    file_writer.write_markdown_file()
+
 if __name__ == '__main__':
     create_files()
+
+
+# python3 metap_script.py -d "python/src" -t "ZM" -r
--- python/src/metaprogramming_r.py
+++ python/src/metaprogramming_r.py
@@ -31,111 +31,112 @@ class RFileWriter():
 
 
         content = f"""
-        library("vsn")
-        library("genefilter")
-        source("r/src/utils.R")
-        source("r/src/pca_utils.R")
-        {self.file_location}
+library("vsn")
+library("genefilter")
+source("r/src/utils.R")
+source("r/src/pca_utils.R")
+{self.file_location}
 
-        times = {times}
+times = {times}
 
 
-        dds <- create_dds('{self.treatment}', data_directory, times, "salmon_quant", {replicate})
-        # Create the data and then save it
-        save(dds, file = glue('r/data/', glue(treatment, "_data.RData")))
+dds <- create_dds('{self.treatment}', data_directory, times, "salmon_quant", {replicate})
+# Create the data and then save it
+save(dds, file = glue('r/data/', glue(treatment, "{self.treatment}_data.RData")))
         """
 
-        file = f'{self.directory}/{self.treatment}_data.R'
+        # file = f'{self.directory}/{self.treatment}_create_data.R'
+        file = f"{self.treatment}_create_data.R"
         with open(file, 'w') as f:
             f.write(content) 
 
-    def write_markdown_file(self, end, start):
+    def write_markdown_file(self):
         outline = self.markdown_outline()
         tabset_detail = "{.tabset}"
         content = f"""
-        {outline}
-        # ZM Analysis {tabset_detail}
+{outline}
+# ZM Analysis {tabset_detail}
 
-        ## TIME - ZM T0 to T16
-        {self.time_analysis(0,16)}
-
-
-        ## TIME - ZM T0 to T20
-        {self.time_analysis(0,20)}
+## TIME - ZM T0 to T16
+{self.time_analysis(0,16)}
 
 
-        ## TIME - ZM T0 to T24
-        {self.time_analysis(0,24)}
+## TIME - ZM T0 to T20
+{self.time_analysis(0,20)}
 
 
-        ## TIME - ZM T0 to T36
-        {self.time_analysis(0,36)}
+## TIME - ZM T0 to T24
+{self.time_analysis(0,24)}
 
 
-        ## TIME - ZM T0 to T48
-        {self.time_analysis(0,48)}
+## TIME - ZM T0 to T36
+{self.time_analysis(0,36)}
+
+
+## TIME - ZM T0 to T48
+{self.time_analysis(0,48)}
 
         """
         
-        file = f'{self.directory}/{self.treatment}_analysis.Rmd'
+        # file = f'{self.directory}/{self.treatment}_analysis.Rmd'
+        file = f'{self.treatment}_analysis.Rmd'
+
         with open(file, 'w') as f:
             f.write(content) 
 
     def time_analysis(self, start, end):
-        timepoint_name = f'timepoint_t{end}_t{start}'
+        timepoint_name = f'timepoint_t{end}_vs_t{start}'
         variable_name = f'results_t{end}_t{start}'
         content = f"""
-        ### Unrestricted
-        #### Summary
-        ```{self._r}
-        {variable_name} <- results(dds, contrast = c("timepoint", "t{end}", "t{start}"))
+### Unrestricted
+#### Summary
+```{self._r}
+{variable_name} <- results(dds, contrast = c("timepoint", "t{end}", "t{start}"))
 
-        {variable_name} = add_annotations_to_results({variable_name})
+{variable_name} = add_annotations_to_results({variable_name})
 
-        summary({variable_name})
+summary({variable_name})
 
-        ```
+```
 
-        ### Restricted
-        #### Summary
-        ```{self._r}
-        {variable_name}_restricted <-
-        results(
-            dds,
-            alpha = 0.1,
-            ## We are looking for genes which have at least doubled, or decreased by more than half
-            lfcThreshold = 1,
-            contrast = c("timepoint", "t{end}", "t{start}")
-        )
-        summary({variable_name}_restricted)
+### Restricted
+#### Summary
+```{self._r}
+{variable_name}_restricted <-
+results(
+    dds,
+    alpha = 0.1,
+    ## We are looking for genes which have at least doubled, or decreased by more than half
+    lfcThreshold = 1,
+    contrast = c("timepoint", "t{end}", "t{start}")
+)
+summary({variable_name}_restricted)
 
-        ```
-        {self.write_ma_plot("%s_restricted" % variable_name)}
+```
+{self.write_ma_plot("%s_restricted" % variable_name)}
 
-        ### With LFC-Shrink
-        It is more useful visualize the MA-plot for the shrunken log2 fold changes, which remove the noise associated with log2 fold changes from low count genes without requiring arbitrary filtering thresholds.
-        #### APE GLM
-        ```{self._r}
+### With LFC-Shrink
+It is more useful visualize the MA-plot for the shrunken log2 fold changes, which remove the noise associated with log2 fold changes from low count genes without requiring arbitrary filtering thresholds.
+#### APE GLM
+```{self._r}
 
-        {variable_name}_shrunk <- lfcShrink(dds, coef="{timepoint_name}", type="apeglm")
-        summary({variable_name}_shrunk)
+{variable_name}_shrunk <- lfcShrink(dds, coef="{timepoint_name}", type="apeglm")
+summary({variable_name}_shrunk)
 
-        ```
+```
 
-        {self.write_ma_plot("%s_shrunk" % variable_name)}
+{self.write_ma_plot("%s_shrunk" % variable_name)}
 
-        #### ashr
-        ```{self._r}
+#### ashr
+```{self._r}
+{variable_name}_shrunk <- lfcShrink(dds, coef="{timepoint_name}", type="ashr")
+summary({variable_name}_shrunk)
 
-        results_t0_to_t48_shrunk <- lfcShrink(dds, coef="timepoint_t48_vs_t0", type="ashr")
-        summary(results_t0_to_t48_shrunk)
+```
 
-        ```
+{self.write_ma_plot("%s_shrunk" % variable_name)}
 
-        {self.write_ma_plot("%s_shrunk" % variable_name)}
-
-        {self.write_p_adjusted_analysis(variable_name)}
-
+{self.write_p_adjusted_analysis(variable_name)}
         """
 
         return content
@@ -143,11 +144,11 @@ class RFileWriter():
     
     def write_ma_plot(self, variable):
         content = """
-        #### MA Plot
-        ```{r}
+#### MA Plot
+```{r}
 
-        plotMA(%s)
-        ```
+plotMA(%s)
+```
 
         """ % variable
 
@@ -155,21 +156,21 @@ class RFileWriter():
 
     def write_p_adjusted_analysis(self, variable):
         content = """
-        ## P-Adjusted Analysis
-        ```{r}
-        ## results_t0_to_t48
-        resSig <- subset(%s, padj < 0.1) 
-        ```
+### P-Adjusted Analysis
+```{r}
+resSig <- subset(%s, padj < 0.1) 
+summary(resSig)
+```
 
-        ### Strongest Down Regulated Genes
-        ```{r}
-        head(resSig[ order(resSig$log2FoldChange), ])
-        ```
+### Strongest Down Regulated Genes
+```{r}
+head(resSig[ order(resSig$log2FoldChange), ])
+```
 
-        ### Strongest up regulation
-        ```{r}
-        head(resSig[ order(resSig$log2FoldChange, decreasing = TRUE), ])
-        ```
+### Strongest up regulation
+```{r}
+head(resSig[ order(resSig$log2FoldChange, decreasing = TRUE), ])
+```
         """ % variable
 
         return content
@@ -179,38 +180,37 @@ class RFileWriter():
     def markdown_outline(self):
         outline = """
 
-        ---
-        title: "Salmon Analysis - ZM"
-        output: html_document
-        ---
+---
+title: "Salmon Analysis - ZM"
+output: html_document
+---
 
-        ```{r setup, include=FALSE}
-            knitr::opts_chunk$set(echo = TRUE)
-            library(DESeq2)
-            library("apeglm")
-            library("ashr")
-            library(org.Hs.eg.db)
+```{r setup, include=FALSE}
+    knitr::opts_chunk$set(echo = TRUE)
+    library(DESeq2)
+    library("apeglm")
+    library("ashr")
+    library(org.Hs.eg.db)
 
-            load("../../../data/ZM_data.RData")
-            dds
-            results <- results(dds)
-            resultsNames(dds)
+    load("../../../data/ZM_data.RData")
+    dds
+    results <- results(dds)
+    resultsNames(dds)
 
-            add_annotations_to_results <- function(res) {
-            ens.str <- substr(rownames(res), 1, 15)
-            res$symbol <- mapIds(org.Hs.eg.db,
-                                keys=ens.str,
-                                column="SYMBOL",
-                                keytype="ENSEMBL",
-                                multiVals="first")
-            res$entrez <- mapIds(org.Hs.eg.db,
-                                keys=ens.str,
-                                column="ENTREZID",
-                                keytype="ENSEMBL",
-                                multiVals="first")
-            return (res)
-            }
-        ```
-        
+    add_annotations_to_results <- function(res) {
+    ens.str <- substr(rownames(res), 1, 15)
+    res$symbol <- mapIds(org.Hs.eg.db,
+                        keys=ens.str,
+                        column="SYMBOL",
+                        keytype="ENSEMBL",
+                        multiVals="first")
+    res$entrez <- mapIds(org.Hs.eg.db,
+                        keys=ens.str,
+                        column="ENTREZID",
+                        keytype="ENSEMBL",
+                        multiVals="first")
+    return (res)
+    }
+```
         """
         return outline
