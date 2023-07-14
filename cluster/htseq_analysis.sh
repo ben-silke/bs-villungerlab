aa267e52342eec7d21c49d364e09e644e94c04e1
bensilke <u6675274@anu.edu.au>
2023-07-14 15:16:52 +0200

    fixed htseq analysis script

--- cluster/htseq_analysis.sh
+++ cluster/htseq_analysis.sh
@@ -13,41 +13,27 @@ treatments=("ZM" "Nutl" "Noc" "Etop" "DHCB")
 ### ATTENTION
 treatments=("ZM")
 
-ls
 # This command should run the STAR mapping process for all treatments/ times.
 for treatment in ${treatments[@]}; do
-    output_htseq="/nobackup/lab_villunger/bsilke/organised/${treatment}/output_htseq_counts"
+    output_htseq="$datadir/organised/${treatment}/output_htseq_counts"
     mkdir $output_htseq
 
     # where do you store stars?
-    night_sky="/nobackup/lab_villunger/bsilke/organised/${treatment}/output_STAR"
+    night_sky="$datadir/organised/${treatment}/output_STAR"
     mkdir $night_sky
     echo "Night Sky: $night_sky"
 
-    input_trimmed_folder="/nobackup/lab_villunger/bsilke/organised/${treatment}/output_trimmed"
+    input_trimmed_folder="$datadir/organised/${treatment}/output_trimmed"
 
     for time in ${times[@]}; do
         for replicate in ${replicates[@]}; do
-            # /Volumes/nobackup/lab_villunger/bsilke/organised/ZM/output_STAR/ZM_24_r4_Aligned.out.sam
+            # /Volumes$datadir/organised/ZM/output_STAR/ZM_24_r4_Aligned.out.sam
             file="${treatment}_${time}_${replicate}_Aligned.out.sam"
-
-            echo "Mapping STARS in $file"
+            input_file="$datadir/organised/ZM/output_STAR/${file}"
+            echo "HTSeq is trying to count in $file"
             output_file="${night_sky}/${file}"
 
-            # --outFileNamePrefix "${night_sky}/${file}"
-
-            # htseq-count [options] <alignment_files> <gtf_file>
-            htseq-count \
-            $file
-
-            htseq-count \
-            -m union \
-            -i gene_name \
-            -a 10 \
-            --stranded=no \
-            $night_sky \              # aligned out files
-            "/nobackup/lab_villunger/bsilke/references/STAR/ensemble_annotation/homo_sapiens/Homo_sapiens.GRCh38.109.gff3" \  # annotation file
-            "$output_htseq/htseq_count_${file}.counts"   # output files
+            htseq-count -m union -i gene_id -a 10 --stranded=no $input_file "$datadir/references/Homo_sapiens.GRCh38.109.gtf" > "$output_htseq/htseq_count_${file}.counts"   # output files
 
 
         done
