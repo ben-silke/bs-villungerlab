6f3f38448461895b0a2744b62126c78bb8f82f09
bensilke <u6675274@anu.edu.au>
2023-07-19 11:51:11 +0200

    adds test script

---
+++ cluster/test_cc.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Mapping step using STAR
+
+treatment=$1
+echo "We are now operating on $treatment"
+
+# Move to the data folder
+datadir="/nobackup/lab_villunger/bsilke"
+cd $datadir
+
+# Include all times, can exclude based upon experiement later.
+times=(0)
+replicates=("r1")
+treatments=("ZM")
+
+# STAR \
+# --runThreadN 2 \
+# --genomeDir ../../data/references/STAR \
+# --readFilesIn "specific to the files"
+
+ls
+# This command should run the STAR mapping process for all treatments/ times.
+# for treatment in ${treatments[@]}; do
+output_htseq="/nobackup/lab_villunger/bsilke/test/"
+mkdir $output_htseq
+
+# where do you store stars?
+night_sky="/nobackup/lab_villunger/bsilke/test"
+mkdir $night_sky
+echo "Night Sky: $night_sky"
+
+input_trimmed_folder="/nobackup/lab_villunger/bsilke/organised/${treatment}/output_trimmed"
+
+for time in ${times[@]}; do
+    for replicate in ${replicates[@]}; do
+        file="${treatment}_${time}_${replicate}"
+
+        echo "Mapping STARS in $file"
+
+        input_trimmed_file="${input_trimmed_folder}/trimmed_fq_${file}.fastq"
+        output_file="${night_sky}/${file}"
+
+        STAR \
+        --runThreadN 4 \
+        --genomeDir /nobackup/lab_villunger/bsilke/gtf_star_index \
+        --readFilesIn "$input_trimmed_file" \
+        --outFileNamePrefix "${night_sky}/${file}"
+        # Now we should delete the old file
+        # rm $input_trimmed_file
+    done
+done
+# done
---
+++ cluster/test_star.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --output /nobackup/lab_villunger/bsilke/logs/star_counts_%j.log # log file location (stdout), %j stands for unique job ID
+#SBATCH --job-name=star_counts
+#SBATCH --partition=longq # job queue where the job is submitted to
+#SBATCH --qos=longq # qos must match the partition
+#SBATCH --nodes=1 # number of physical nodes
+#SBATCH --ntasks=1 # 1 task
+#SBATCH --cpus-per-task=4 # 1 task on 1 CPU
+#SBATCH --time=12:00:00 #
+# Optional parameters
+#SBATCH --mem=400000 # using 400gb of memory
+#SBATCH --error /nobackup/lab_villunger/bsilke/logs/star_counts_%j.err # error log file location (stderr), %j stands for unique job ID
+#SBATCH --mail-type=end # send an email when this job ends
+#SBATCH --mail-user=bsilke@cemm.at # email your CeMM account
+
+# (Optional) Examples of the Slurm environmental variables. Provide additional information
+echo "======================"
+echo $SLURM_SUBMIT_DIR
+echo $SLURM_JOB_NAME
+echo $SLURM_JOB_PARTITION
+echo $SLURM_NTASKS
+echo $SLURM_NPROCS
+echo $SLURM_JOB_ID
+echo $SLURM_JOB_NUM_NODES
+echo $SLURM_NODELIST
+echo $SLURM_CPUS_ON_NODE
+echo "======================"
+
+# *** setup environment ***
+# load the required environmental modules that your script depends upon
+module load STAR
+# *** run the job ***
+date
+./test_cc.sh "ZM"
+date
+
+seff $SLURM_JOB_ID
