82cf952249525197db8124406a502f3dade69c68
bensilke <u6675274@anu.edu.au>
2023-07-24 09:54:12 +0200

    adds scrath cp commands

--- cluster/scratch/copy_to_cluster.sh
+++ cluster/scratch/copy_to_cluster.sh
@@ -1,1 +1,8 @@
 scp -r cluster/ login://nobackup/lab_villunger/bsilke/repo/bs-villungerlab
+
+## copying files for all treatments
+scp -r login://nobackup/lab_villunger/bsilke/organised/ZM/output_htseq_counts output_htseq_unique/ZM
+scp -r login://nobackup/lab_villunger/bsilke/organised/Etop/output_htseq_counts output_htseq_unique/Etop
+scp -r login://nobackup/lab_villunger/bsilke/organised/DHCB/output_htseq_counts output_htseq_unique/DHCB
+scp -r login://nobackup/lab_villunger/bsilke/organised/Noc/output_htseq_counts output_htseq_unique/Noc
+scp -r login://nobackup/lab_villunger/bsilke/organised/Nutl/output_htseq_counts output_htseq_unique/Nutl
