4a71aee1da5330c3055fe33e1f9b56e237972c8e
bensilke <u6675274@anu.edu.au>
2023-07-27 09:59:10 +0200

    updated plot saving size

--- r/src/star_analysis/encode_local_files/gene_selection/ZM_gene_selection_n25.R
+++ r/src/star_analysis/encode_local_files/gene_selection/ZM_gene_selection_n25.R
@@ -26,6 +26,11 @@ source("~/bs-villungerlab/r/src/star_analysis/star_utils.R")
 source("~/bs-villungerlab/r/src/star_analysis/gene_selection_utils.R")
 ######
 load('~/bs-villungerlab/results/output_encode_1to6/ZM_star_data.RData')
+
+dpi = 500
+width_in <- 20
+height_in <- 20
+
 dds_ZM <- ddseq_ZM
 
 results_ZM_t16_df <- return_results(dds_ZM, "timepoint_t16_vs_t0", "_16")
@@ -35,26 +40,27 @@ results_ZM_t36_df <- return_results(dds_ZM, "timepoint_t36_vs_t0", "_36")
 results_ZM_t48_df <- return_results(dds_ZM, "timepoint_t48_vs_t0", "_48")
 df <- results_ZM_t24_df
 
+all_df_merged_df <- merge_all_data(results_ZM_t48_df, results_ZM_t16_df, results_ZM_t20_df, results_ZM_t24_df, results_ZM_t36_df, 'results/output_encode/ZM/all_ZM_gene_regulation_data.csv', 'full_join')
+df <- all_df_merged_df
 #####
-#now you can apply filtering
-upr_df_sorted <- df[order(-df$log2FoldChange), ]
+upr_df_sorted <- df[order(-df$log2FoldChange_24), ]
 
 # set the number of results which you want
 upr_top <- head(upr_df_sorted, 25)
-upr_top_merged_df <- merge_all_data(upr_top, results_ZM_t16_df, results_ZM_t20_df, results_ZM_t36_df, results_ZM_t48_df, 'results/output_encode/ZM/n25generegulation/ZM_gene_regulation_data.csv')
-upr_top_long_df <- make_longdf_for_plot(upr_top_merged_df, 24)
-upr_plot <- plot_longdf(upr_top_long_df, "ZM upregulated genes: n25 | t24")
+upr_top_long_df <- make_longdf_for_plot(upr_top, 24)
+upr_plot <- plot_longdf(upr_top_long_df, "ZM upregulated genes: (3n) n25 | t24")
 upr_plot
-ggsave(filename = "results/output_encode/ZM/n25generegulation/ZM_upregulated_genes.pdf", plot = upr_plot)
+ggsave(filename = "results/output_encode/ZM/n25generegulation/ZM_upregulated_genes.pdf", plot = upr_plot, dpi=dpi, width=width_in, height=height_in)
 
-downr_df_sorted <- df[order(df$log2FoldChange), ]
+downr_df_sorted <- df[order(df$df$log2FoldChange_24), ]
 # set the number of results which you want
 downr_top <- head(downr_df_sorted, 25)
 downr_top_merged_df <- merge_all_data(downr_top, results_ZM_t16_df, results_ZM_t20_df, results_ZM_t36_df, results_ZM_t48_df, 'results/output_encode/ZM/n25generegulation/ZM_gene_regulation_data.csv')
 downr_top_long_df <- make_longdf_for_plot(downr_top_merged_df, 24)
-downr_plot <- plot_longdf(downr_top_long_df, "ZM downregulated genes: n25 | t24")
+downr_plot <- plot_longdf(downr_top_long_df, "ZM downregulated genes: (3n) n25 | t24")
 downr_plot
-ggsave(filename = "results/output_encode/ZM/n25generegulation/ZM_downregulated_genes.pdf", plot = downr_plot)
+ggsave(filename = "results/output_encode/ZM/n25generegulation/ZM_downregulated_genes.pdf", plot = downr_plot, dpi=dpi, width=width_in, height=height_in)
+
 
 #####
 df 
@@ -63,23 +69,6 @@ dim(df)
 all_df_merged_df <- merge_all_data(results_ZM_t48_df, results_ZM_t16_df, results_ZM_t20_df, results_ZM_t24_df, results_ZM_t36_df, 'results/output_encode/ZM/all_ZM_gene_regulation_data.csv', 'full_join')
 dim(all_df_merged_df)
 all_df_merged_df$log2FoldChange_16
-
-
-#######
-# subset_df <- subset(all_df_merged_df, !is.na(log2FoldChange_16))
-# subset_df <- subset(subset_df, !is.na(log2FoldChange_20))
-# subset_df <- subset(subset_df, !is.na(log2FoldChange_36))
-# subset_df <- subset(subset_df, !is.na(log2FoldChange_48))
-# 
-# # 'results/output_encode/ZM/all_ZM_gene_regulation_data.csv'
-# write.csv(subset_df, file = "results/output_encode/ZM/ZM_all_timepoints.csv")
-# subset_long_df <- make_longdf_for_plot(subset_df, 24)
-# subset_plot <- plot_longdf(subset_long_df, "ZM genes (all timepoints)")
-# subset_plot
-
-# ggsave(filename = "results/output_encode/ZM/ZM_all_timepoints.pdf", plot = subset_plot)
-
-# upr_top_long_df <- make_longdf_for_plot(upr_top_merged_df, 24)
 
 
 all_df_merged_df
@@ -97,7 +86,7 @@ semi_subset_plot <- plot_longdf(semisubset_long_df, "ZM genes (20,24,36 timepoin
 semi_subset_plot
 
 write.csv(semi_subset_df, file = "results/output_encode/ZM/ZM_middle_timepoints_signature.csv")
-ggsave(filename = "results/output_encode/ZM/ZM_middle_timepoints.pdf", plot = semi_subset_plot)
+ggsave(filename = "results/output_encode/ZM/ZM_middle_timepoints.pdf", plot = semi_subset_plot, dpi=dpi, width=width_in, height=height_in)
 ######
 
 full_signature <- subset(all_df_merged_df, !is.na(log2FoldChange_16))
@@ -107,6 +96,26 @@ full_signature <- subset(full_signature, !is.na(log2FoldChange_36))
 full_signature <- subset(full_signature, !is.na(log2FoldChange_48))
                        
 write.csv(full_signature, file = "results/output_encode/ZM/ZM_full_signature_signature.csv")
+full_signature_threshold <- full_signature[
+  any(abs(full_signature$log2FoldChange_16)>1 | 
+        abs(full_signature$log2FoldChange_20)>1 | 
+        abs(full_signature$log2FoldChange_24)>1 | 
+        abs(full_signature$log2FoldChange_36)>1 |
+        abs(full_signature$log2FoldChange_48)>1), ]
+
+full_signature_threshold_long_df <- make_longdf_for_plot(full_signature_threshold, 24)
+plot_title <- "Full Signature ZM treatment Signature"
+full_signature_plot <- ggplot(full_signature_threshold_long_df, aes(x = Timepoint, y = log2foldchange, shape = symbol, group = symbol, color = symbol)) +
+  geom_point() +
+  geom_line() +
+  labs(title = plot_title,
+       x = "time(hrs)",
+       y = expression(paste(log[2](x), ' fold change'))) +
+  theme(plot.title = element_text(hjust = 0.5), # Center the title
+        plot.title.position = "plot")
+full_signature_plot
+
+ggsave(filename = "results/output_encode/ZM/ZM_full_signature_plot.pdf", plot = two_p, dpi=dpi, width=width_in, height=height_in)
 
 
 df <- all_df_merged_df
@@ -114,6 +123,7 @@ df
 dim(df)
 colnames(df)
 # subset the data to only include data which is greater than 1 l2foldchange
+
 subset_increase <- df[
   any(abs(df$log2FoldChange_16)>1 | 
   abs(df$log2FoldChange_20)>1 | 
@@ -121,9 +131,18 @@ subset_increase <- df[
   abs(df$log2FoldChange_36)>1 |
   abs(df$log2FoldChange_48)>1), ]
 
+
+
+# dim(df)
+# dim(subset_increase)
+# subset_increase <- df[abs(df$log2FoldChange_16)>1, ]
+# subset_increase <- subset_increase[abs(subset_increase$log2FoldChange_20)>1, ]
+# subset_increase <- subset_increase[abs(subset_increase$log2FoldChange_24)>1, ]
+# subset_increase <- subset_increase[abs(subset_increase$log2FoldChange_36)>1, ]
+# subset_increase <- subset_increase[abs(subset_increase$log2FoldChange_48)>1, ]
+
 dim(df)
 dim(subset_increase)
-# subset_df <- df[apply(df, 1, function(x) any(x["col1"] > 5, x["col2"] < 15)), ]
 
 df <- subset_increase
 
@@ -134,6 +153,9 @@ two_consecutivesubset_df <- df[
   !is.na(df$log2FoldChange_36) & !is.na(df$log2FoldChange_48)
   , ]
 
+dim(df)
+dim(two_consecutivesubset_df)
+
 write.csv(two_consecutivesubset_df, file = "results/output_encode/ZM/ZM_gene_signature_two_points.csv")
 two_consecutivesubset_df_increase <- subset(two_consecutivesubset_df, (log2FoldChange_24 > 0 | log2FoldChange_16 > 0 | log2FoldChange_20 > 0 | log2FoldChange_36 > 0 | log2FoldChange_48 > 0))
 dim(two_consecutivesubset_df_increase)
@@ -141,8 +163,6 @@ write.csv(two_consecutivesubset_df_increase, file = "results/output_encode/ZM/ZM
 
 noquote_two_consecutivesubset_df_increase <- noquote(two_consecutivesubset_df_increase$symbol_48)
 write(noquote_two_consecutivesubset_df_increase, file = "results/output_encode/ZM/ZM_gene_signature_two_points_increase.txt")
-
-
 
 two_consecutivesubset_df_decrease <- subset(two_consecutivesubset_df, (log2FoldChange_24 < 0 | log2FoldChange_16 < 0 | log2FoldChange_20 < 0 | log2FoldChange_36 < 0 | log2FoldChange_48 < 0))
 dim(two_consecutivesubset_df_decrease)
@@ -154,11 +174,12 @@ write(noquote_two_consecutivesubset_df_decrease, file = "results/output_encode/Z
 
 dim(two_consecutivesubset_df)
 
-three_consecutivesubset_df <- df[
-  !is.na(df$log2FoldChange_16) & !is.na(df$log2FoldChange_20) & !is.na(df$log2FoldChange_24) |
-    !is.na(df$log2FoldChange_20) & !is.na(df$log2FoldChange_24) & !is.na(df$log2FoldChange_36) |
-    !is.na(df$log2FoldChange_24) & !is.na(df$log2FoldChange_36) & !is.na(df$log2FoldChange_48)
-  , ]
+three_consecutivesubset_df <- df[(!is.na(df$log2FoldChange_16) & !is.na(df$log2FoldChange_20) & !is.na(df$log2FoldChange_24) |
+  !is.na(df$log2FoldChange_20) & !is.na(df$log2FoldChange_24) & !is.na(df$log2FoldChange_36) |
+  !is.na(df$log2FoldChange_24) & !is.na(df$log2FoldChange_36) & !is.na(df$log2FoldChange_48)), ]
+
+# dim(three_consecutivesubset_df)
+
 
 write.csv(three_consecutivesubset_df, file = "results/output_encode/ZM/ZM_gene_signature_three_points.csv")
 
@@ -192,7 +213,7 @@ two_p <- ggplot(two_consecutive_values_long_df, aes(x = Timepoint, y = log2foldc
         legend.position = "none")
 two_p
 
-ggsave(filename = "results/output_encode/ZM/ZM_plot_two_consecutive_values.pdf", plot = two_p)
+ggsave(filename = "results/output_encode/ZM/ZM_plot_two_consecutive_values.pdf", plot = two_p, dpi=dpi, width=width_in, height=height_in)
 
 
 three_consecutivesubset_long_df <- make_longdf_for_plot(three_consecutivesubset_df, 24)
@@ -207,6 +228,71 @@ three_p <- ggplot(three_consecutivesubset_long_df, aes(x = Timepoint, y = log2fo
         plot.title.position = "plot",
         legend.position = "none")
 
-ggsave(filename = "results/output_encode/ZM/ZM_plot_three_consecutive_values.pdf", plot = three_p)
+ggsave(filename = "results/output_encode/ZM/ZM_plot_three_consecutive_values.pdf", plot = three_p, dpi=dpi, width=width_in, height=height_in)
 
 three_p
+
+df <- three_consecutivesubset_df
+#now you can apply filtering
+upr_df_sorted <- df[order(-df$log2FoldChange_24), ]
+
+# set the number of results which you want
+upr_top <- head(upr_df_sorted, 25)
+upr_top_merged_df <- merge_all_data(upr_top, results_ZM_t16_df, results_ZM_t20_df, results_ZM_t36_df, results_ZM_t48_df, 'results/output_encode/ZM/n25generegulation/ZM_gene_regulation_data.csv')
+upr_top_long_df <- make_longdf_for_plot(upr_top_merged_df, 24)
+upr_plot <- plot_longdf(upr_top_long_df, "ZM upregulated genes: (3n) n25 | t24")
+upr_plot
+ggsave(filename = "results/output_encode/ZM/n25generegulation/ZM_three_consecutive_upregulated_genes.pdf", plot = upr_plot, dpi=dpi, width=width_in, height=height_in)
+
+downr_df_sorted <- df[order(df$df$log2FoldChange_24), ]
+# set the number of results which you want
+downr_top <- head(downr_df_sorted, 25)
+downr_top_merged_df <- merge_all_data(downr_top, results_ZM_t16_df, results_ZM_t20_df, results_ZM_t36_df, results_ZM_t48_df, 'results/output_encode/ZM/n25generegulation/ZM_gene_regulation_data.csv')
+downr_top_long_df <- make_longdf_for_plot(downr_top_merged_df, 24)
+downr_plot <- plot_longdf(downr_top_long_df, "ZM downregulated genes: (3n) n25 | t24")
+downr_plot
+ggsave(filename = "results/output_encode/ZM/n25generegulation/ZM_three_consecutive_downregulated_genes.pdf", plot = downr_plot, dpi=dpi, width=width_in, height=height_in)
+
+
+
+
+
+dim(df)
+#now you can apply filtering
+upr_df_sorted <- df[order(-df$log2FoldChange_24), ]
+
+# set the number of results which you want
+upr_top <- head(upr_df_sorted, 25)
+upr_top_merged_df <- merge_all_data(upr_top, results_ZM_t16_df, results_ZM_t20_df, results_ZM_t36_df, results_ZM_t48_df, 'results/output_encode/ZM/n25generegulation/ZM_full_5n_gene_regulation_data.csv')
+
+df <- upr_top_merged_df
+df <- subset(df, !is.na(log2FoldChange_16))
+df <- subset(df, !is.na(log2FoldChange_20))
+df <- subset(df, !is.na(log2FoldChange_24))
+df <- subset(df, !is.na(log2FoldChange_36))
+df <- subset(df, !is.na(log2FoldChange_48))
+
+upr_top_long_df <- make_longdf_for_plot(df, 24)
+upr_plot <- plot_longdf(upr_top_long_df, "ZM upregulated genes: (5n) n25 | t24")
+upr_plot
+ggsave(filename = "results/output_encode/ZM/n25generegulation/ZM_full_upregulated_genes.pdf", plot = upr_plot, dpi=dpi, width=width_in, height=height_in)
+
+downr_df_sorted <- df[order(df$df$log2FoldChange_24), ]
+# set the number of results which you want
+downr_top <- head(downr_df_sorted, 25)
+downr_top_merged_df <- merge_all_data(downr_top, results_ZM_t16_df, results_ZM_t20_df, results_ZM_t36_df, results_ZM_t48_df, 'results/output_encode/ZM/n25generegulation/ZM_gene_regulation_data.csv')
+
+df <- downr_top_merged_df
+df <- subset(df, !is.na(log2FoldChange_16))
+df <- subset(df, !is.na(log2FoldChange_20))
+df <- subset(df, !is.na(log2FoldChange_24))
+df <- subset(df, !is.na(log2FoldChange_36))
+df <- subset(df, !is.na(log2FoldChange_48))
+
+downr_top_long_df <- make_longdf_for_plot(df, 24)
+downr_plot <- plot_longdf(downr_top_long_df, "ZM downregulated genes: (3n) n25 | t24")
+downr_plot
+ggsave(filename = "results/output_encode/ZM/n25generegulation/ZM_full_consecutive_downregulated_genes.pdf", plot = downr_plot, dpi=dpi, width=width_in, height=height_in)
+
+
+
