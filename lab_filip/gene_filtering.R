52917e53d72eb88531625b1834bc9289de0210b1
bensilke <u6675274@anu.edu.au>
2023-08-04 15:48:45 +0200

    adds gene long df to filip work

--- lab_filip/gene_filtering.R
+++ lab_filip/gene_filtering.R
@@ -59,23 +59,71 @@ subset_merged_df <- subset(subset_merged_df, padj_30 < 0.05)
 
 View(subset_merged_df)
 
-# Post Filtering
-noquote_gene_list <- noquote(subset_merged_df$gene_id)
-noquote_gene_list <- c('gene_name', noquote_gene_list)
-write(noquote_gene_list, file = "results/lab_filip/gene_list_padj005.txt")
+# Post Filtering _ split by increase and decrease
+########
 
 
+subset_increase <- subset(merged_df,log2FoldChange.shrunk_24 > 0 )
+subset_decrease <- subset(merged_df,log2FoldChange.shrunk_24 < 0 )
 
-df_long_subset_targets <- generate_complete_long_df(subset_merged_df, 24)
-View(df_long_subset_targets)
+noquote_gene_list_increase <- noquote(subset_increase$gene_id)
+noquote_gene_list_decrease <- noquote(subset_decrease$gene_id)
 
-headers <- colnames(df_long_subset_targets)
-headers[1] ='symbol'
-colnames(df_long_subset_targets) <- headers
+noquote_gene_list_increase <- c('gene_name', noquote_gene_list_increase)
+noquote_gene_list_decrease <- c('gene_name', noquote_gene_list_decrease)
 
+write(noquote_gene_list_increase, file = "results/lab_filip/increase_gene_list_padj005.txt")
+write(noquote_gene_list_decrease, file = "results/lab_filip/decrease_gene_list_padj005.txt")
+
+#### Generate long Df
+######
+colnames(merged_df)
+
+ndf <- data.frame(names<-merged_df$gene_id)
+ndf$n_12 <- merged_df$log2FoldChange.shrunk_12
+ndf$n_24 <- merged_df$log2FoldChange.shrunk_24
+ndf$n_30 <- merged_df$log2FoldChange.shrunk_30
+ndf$symbol <- merged_df$gene_id
+head(ndf)
+print(ndf)
+
+df_long_time <- ndf %>%
+  pivot_longer(
+    cols = starts_with("n_"), # Select columns that start with "n_"
+    names_to = "Timepoint", # The names of these columns will go into a new column "Timepoint"
+    values_to = "log2foldchange" # The values will go into a new column "log2change"
+  ) %>%
+  mutate(Timepoint = str_extract(Timepoint, "\\d+"), # Extract numeric part from Timepoint values
+         Timepoint = as.numeric(Timepoint)) # Convert Timepoint values to numeric
+
+
+## correct for padj
+padj_df <- data.frame(names<-merged_df$gene_id)
+padj_df$padj_12 <- merged_df$padj_12
+padj_df$padj_24 <- merged_df$padj_24
+padj_df$padj_30 <- merged_df$padj_30
+padj_df$symbol <- merged_df$gene_id
+
+df_long_padj <- padj_df %>%
+  pivot_longer(
+    cols = starts_with("padj_"), # Select columns that start with "n_"
+    names_to = "Timepoint", # The names of these columns will go into a new column "Timepoint"
+    values_to = "padj" # The values will go into a new column "padj"
+  ) %>%
+  mutate(Timepoint = str_extract(Timepoint, "\\d+"), # Extract numeric part from Timepoint values
+         Timepoint = as.numeric(Timepoint)) # Convert Timepoint values to numeric
+
+print(df_long_padj)
+df_long <- merge(df_long_time, df_long_padj)
+df_long$names....merged_df.gene_id <- NULL
+colnames(df_long)
+
+# headers <- colnames(df_long)
+# headers[1] ='symbol'
+# colnames(df_long_subset_targets) <- headers
 
 df_long_subset_targets_plot <- plot_ly(
-  df_long_subset_targets,
+  df_long,
   x = ~Timepoint,
   y = ~log2foldchange,
   mode = "markers+lines",
@@ -89,3 +137,5 @@ df_long_subset_targets_plot <- plot_ly(
 df_long_subset_targets_plot
 saveWidget(df_long_subset_targets_plot, "results/lab_filip/transcript_data.html")
 
+
+##########
