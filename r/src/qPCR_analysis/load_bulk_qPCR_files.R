95d09d45fd01d5e93cada247216648eb826b2664
bensilke <u6675274@anu.edu.au>
2023-08-21 15:43:57 +0200

    adds code to load data from second qPCR round

--- r/src/qPCR_analysis/load_bulk_qPCR_files.R
+++ r/src/qPCR_analysis/load_bulk_qPCR_files.R
@@ -14,43 +14,58 @@ source("~/bs-villungerlab/r/src/gene_selection_utils.R")
 
 source("~/bs-villungerlab/r/src/qPCR_utils.R")
 # 
-dir.create('lab_work/qPCR/full_output')
+dir.create('lab_work/qPCR/fulloutput')
 EXTENSION = "lab_work/raw_data/"
-# /Users/bsilke/bs-villungerlab/lab_work/raw_data/qPCR_nocOld_zmOld_zm-19_noc5.xlsx
 file_one = "qPCR_nocOld_zmOld_zm-19_noc5.xlsx"
 # /Users/bsilke/bs-villungerlab/lab_work/raw_data/qPCR_v2_nocOld_zmOld_zm19_noc5.xlsx
 file_two = "qPCR_v2_nocOld_zmOld_zm19_noc5.xlsx"
 # /Users/bsilke/bs-villungerlab/lab_work/raw_data/qPCR_zmNew_nocNew_nocPlus_zmPlus.xlsx
 file_three = "qPCR_zmNew_nocNew_nocPlus_zmPlus.xlsx"
 
-df_one = read_excel(file.path(EXTENSION, file_one), sheet='0')
+# df_one = read_excel(file.path(EXTENSION, file_one), sheet='0')
 # df_two = read_excel(file.path(EXTENSION, file_two), sheet='0')
 # df_three = read_excel(file.path(EXTENSION, file_three), sheet='0')
 
-View(df_one)
-
-colnames(df_one)
-
-df <- df_one %>% dplyr::select('Target', 'Sample', 'adj_cq')
-cols <- colnames(df)
-cols[3] = 'Cq'
-colnames(df) = cols
-colnames(df)
-View(df)
+# View(df_one)
+# 
+# colnames(df_one)
+# 
+# df <- df_one %>% dplyr::select('Target', 'Sample', 'adj_cq')
+# cols <- colnames(df)
+# cols[3] = 'Cq'
+# colnames(df) = cols
+# colnames(df)
+# View(df)
 targets = c('BMF', "FOXM1", "SQSTM1", "NINJ1", "ZMAT3", "PHLDA3", "CCNA2", "CDCA8", "CDC25A", "AURKB", "ARID5B", "ANKRD1")
-full_df <- data.frame()
-biological_replicate='zm_old'
-for (target in targets) {
-  long_df <- create_long_df_for_sample(df, biological_replicate, target)
-  full_df <- rbind(full_df, long_df)
+
+save_html_for_treatment <- function(file, biological_replicate, targets, save_location, title, treatment, extension="lab_work/raw_data/") {
+  df <- read_excel(file.path(EXTENSION, file), sheet='0')
+  df <- df %>% dplyr::select('Target', 'Sample', 'adj_cq')
+  cols <- colnames(df)
+  cols[3] = 'Cq'
+  colnames(df) = cols
+  
+  full_df <- data.frame()
+  for (target in targets) {
+    long_df <- create_longdf(df, biological_replicate, target)
+    full_df <- rbind(full_df, long_df)
+  }
+  df_long_subset_targets_plot <- plot_ly(
+    full_df,
+    x = ~timepoint,
+    y = ~avg_ddct2,
+    mode = "markers+lines",
+    text = ~paste("T: ", timepoint, "<br>avg_ddct2: ", avg_ddct2),
+    hoverinfo = "text",
+    split= ~target_name
+  ) %>%
+    layout(title=title)
+  saveWidget(df_long_subset_targets_plot, save_location)
+  full_df$treatment = treatment
+  return(full_df)
 }
 
-full_df
-
-# control="GAPDH"
-# sample_name = 'zm_old'
-# target_name ='SQSTM1'
-create_long_df_for_sample <- function(df, sample_name, target_name, control="GAPDH") {
+create_longdf <- function(df, sample_name, target_name, control="GAPDH") {
   
   # SETUP control dataframe
   control_df <- subset(df, Target==control)
@@ -123,16 +138,21 @@ create_long_df_for_sample <- function(df, sample_name, target_name, control="GAP
   long_df
   return (long_df)
 }
+# qPCR_nocOld_zmOld_zm-19_noc5.xlsx
+df_one <- save_html_for_treatment("qPCR_nocOld_zmOld_zm-19_noc5.xlsx", 'noc_old', targets, "lab_work/qPCR/fulloutput/Noc_old_pcr.html", "Nocodazole Treatment - old", "Noc")
+df_two <- save_html_for_treatment("qPCR_nocOld_zmOld_zm-19_noc5.xlsx", 'zm_old', targets, "lab_work/qPCR/fulloutput/zm_old_pcr.html", "ZM Treatment - old", "ZM")
+df_three <- save_html_for_treatment("qPCR_nocOld_zmOld_zm-19_noc5.xlsx", 'zm_19', targets, "lab_work/qPCR/fulloutput/zm_19_all_pcr.html", "ZM Treatment - zm19", "ZM")
+df_four <- save_html_for_treatment("qPCR_nocOld_zmOld_zm-19_noc5.xlsx", 'noc_5', targets, "lab_work/qPCR/fulloutput/noc_5_pcr.html", "Nocodazole Treatment - zm5", "Noc")
 
-title = 'TEST'
-df_long_subset_targets_plot <- plot_ly(
-  full_df,
-  x = ~timepoint,
-  y = ~avg_ddct2,
-  mode = "markers+lines",
-  text = ~paste("T: ", timepoint, "<br>avg_ddct2: ", avg_ddct2),
-  hoverinfo = "text",
-  split= ~target_name
-) %>%
-  layout(title=target_name)
-df_long_subset_targets_plot
+# qPCR_v2_nocOld_zmOld_zm19_noc5.xlsx
+df_5 <- save_html_for_treatment("qPCR_v2_nocOld_zmOld_zm19_noc5.xlsx", 'noc_old', targets, "lab_work/qPCR/fulloutput/Noc_old_v2_pcr.html", "Nocodazole Treatment - old", "Noc")
+df_6 <- save_html_for_treatment("qPCR_v2_nocOld_zmOld_zm19_noc5.xlsx", 'zm_old', targets, "lab_work/qPCR/fulloutput/zm_old_v2_pcr.html", "ZM Treatment - old", "ZM")
+df_7 <- save_html_for_treatment("qPCR_v2_nocOld_zmOld_zm19_noc5.xlsx", 'zm_19', targets, "lab_work/qPCR/fulloutput/zm_19_v2_pcr.html", "ZM Treatment - zm19", "ZM")
+df_8 <- save_html_for_treatment("qPCR_v2_nocOld_zmOld_zm19_noc5.xlsx", 'noc_p5', targets, "lab_work/qPCR/fulloutput/noc_5_v2_pcr.html", "Nocodazole Treatment - zm5", "Noc")
+
+
+file_three = "qPCR_zmNew_nocNew_nocPlus_zmPlus.xlsx"
+df_9 <- save_html_for_treatment(file_three, 'noc_plus', targets, "lab_work/qPCR/fulloutput/Noc_plus_pcr.html", "Nocodazole Treatment - plus", "Noc")
+df_10 <- save_html_for_treatment(file_three, 'zm_plus', targets, "lab_work/qPCR/fulloutput/zm_plus_pcr.html", "ZM Treatment - plus", "ZM")
+df_11<- save_html_for_treatment(file_three, 'zm_new', targets, "lab_work/qPCR/fulloutput/zm_new_pcr.html", "ZM Treatment - new", "ZM")
+df_12 <- save_html_for_treatment(file_three, 'noc_new', targets, "lab_work/qPCR/fulloutput/noc_new_pcr.html", "Nocodazole Treatment - new", "Noc")
